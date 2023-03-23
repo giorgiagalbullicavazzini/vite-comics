03ee2afcd29282336239f42c00a08d028f7e3b6d
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-03-23 17:50:47 +0100

    footer top layout

---
+++ src/components/footer/FooterList.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <h3>Title</h3>
+    <ul>
+        <li>List</li>
+        <li>List</li>
+        <li>List</li>
+        <li>List</li>
+        <li>List</li>
+    </ul>
+</template>
+
+<style scoped>
+h3 {
+    color: #ffffff;
+}
+
+ul {
+    color: #7c7e80;
+    list-style: none;
+    margin: 10px 0;
+}
+</style>
--- src/components/footer/FooterTop.vue
+++ src/components/footer/FooterTop.vue
@@ -1,15 +1,31 @@
 <script>
+import FooterList from './FooterList.vue';
+
 export default {
     data() {
         return {
         }
+    },
+    components: {
+        FooterList
     }
 }
 </script>
 
 <template>
     <div id="footer-top">
-        <div class="container"></div>
+        <div class="container">
+            <div class="left column">
+                <FooterList></FooterList>
+                <FooterList></FooterList>
+            </div>
+            <div class="center column">
+                <FooterList></FooterList>
+            </div>
+            <div class="right column">
+                <FooterList></FooterList>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -19,6 +35,11 @@ export default {
 }
 
 .container {
+    display: flex;
     height: 370px;
+}
+
+.column {
+    margin: 30px 20px 30px 0;
 }
 </style>
