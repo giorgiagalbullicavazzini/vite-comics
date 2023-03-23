1c55984a3a88fa2fb748e537c18b0a791d4ebc9b
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-03-23 18:40:06 +0100

    removed FooterList component and added footer top content

--- src/components/footer/FooterTop.vue
+++ src/components/footer/FooterTop.vue
@@ -1,13 +1,41 @@
 <script>
-import FooterList from './FooterList.vue';
-
 export default {
     data() {
         return {
+            comics: [
+                "Characters",
+                "Comics",
+                "Movies",
+                "TV",
+                "Games",
+                "Videos",
+                "News"
+            ],
+            shop: [
+                "Shop DC",
+                "Shop DC Collectibles"
+            ],
+            dc: [
+                "Terms Of Use",
+                "Privacy policy (New)",
+                "Ad Choices",
+                "Advertising",
+                "Jobs",
+                "Subscriptions",
+                "Talent Workshops",
+                "CPSC Certificates",
+                "Ratings",
+                "Shop Help",
+                "Contact Us"
+            ],
+            sites: [
+                "DC",
+                "MAD Magazine",
+                "DC Kids",
+                "DC Universe",
+                "DC Power Visa"
+            ]
         }
-    },
-    components: {
-        FooterList
     }
 }
 </script>
@@ -16,14 +44,27 @@ export default {
     <div id="footer-top">
         <div class="container">
             <div class="left column">
-                <FooterList></FooterList>
-                <FooterList></FooterList>
+                <h3>DC Comics</h3>
+                <ul>
+                    <li v-for="comic in comics"><a href="#">{{ comic }}</a></li>
+                </ul>
+
+                <h3>Shop</h3>
+                <ul>
+                    <li v-for="shop in shop"><a href="#">{{ shop }}</a></li>
+                </ul>
             </div>
             <div class="center column">
-                <FooterList></FooterList>
+                <h3>DC</h3>
+                <ul>
+                    <li v-for="dc in dc"><a href="#">{{ dc }}</a></li>
+                </ul>
             </div>
             <div class="right column">
-                <FooterList></FooterList>
+                <h3>Sites</h3>
+                <ul>
+                    <li v-for="site in sites"><a href="#">{{ site }}</a></li>
+                </ul>
             </div>
         </div>
     </div>
@@ -41,5 +82,15 @@ export default {
 
 .column {
     margin: 30px 20px 30px 0;
+}
+
+h3 {
+    color: #ffffff;
+}
+
+ul {
+    color: #7c7e80;
+    list-style: none;
+    margin: 10px 0;
 }
 </style>
