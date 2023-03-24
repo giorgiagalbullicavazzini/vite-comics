89a1fab5ca36391f4b84e765bd09ac70281c5895
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-03-24 19:03:10 +0100

    comics props creation

---
+++ src/components/main/ComicCard.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    name: 'ComicCard',
+    props: {
+        img: String,
+        title: String
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <div class="image">
+            <img :src="img" :alt="title">
+        </div>
+        <h3>{{ title }}</h3>
+    </div>
+</template>
+
+<style scoped>
+.image {
+    aspect-ratio: 1;
+    width: 100%;
+}
+</style>
